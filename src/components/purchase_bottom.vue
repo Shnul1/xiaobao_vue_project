db34e684490cd076b30bd0af07b1113af5a481ce
孙浩 <898241954@qq.com>
2020-12-22 18:59:37 +0800

    first commit

---
+++ src/components/purchase_bottom.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="purchase-bottom">
+        <div class="shopping-cart">
+            <div class="shopping-cart-icon"></div>
+            <div class="shopping-cart-text">购物车</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.purchase-bottom{
+    display: flex;
+    width: 375px;
+    height: 68px;
+    background: #FFFFFF;
+    box-shadow: 0px -10px 30px 0px rgba(0, 0, 0, 0.03);
+    position: fixed;
+    .shopping-cart{
+        .shopping-cart-icon{
+            width: 34px;
+            height: 34px;
+        }
+
+        .shopping-cart-text{
+            height: 14px;
+            font-size: 12px;
+            font-family: PingFangSC-Regular, PingFang SC;
+            font-weight: 400;
+            color: #969799;
+            line-height: 14px;
+        }
+    }
+
+
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/vant'
+import './assets/css/global.css'
+import product from './views/product_detail'
 
 Vue.config.productionTip = false
+Vue.component('product', product)
 
 new Vue({
   router,
